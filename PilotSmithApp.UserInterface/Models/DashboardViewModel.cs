4601296638cd66e83023923f2e2eda71962f1b06
Suvaneeth <suvaneeth@gmail.com>
2018-04-17 19:59:12 +0530

    dto and vm added for dashboard salessummary

---
+++ PilotSmithApp.UserInterface/Models/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PilotSmithApp.UserInterface.Models
+{
+    public class DashboardViewModel
+    {
+    }
+    public class SalesSummaryList
+    {
+        public List<SalesSummaryViewModel> SalesSummaryVMList { get; set; }
+    }
+}
---
+++ PilotSmithApp.UserInterface/Models/SalesInvoiceViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PilotSmithApp.UserInterface.Models
+{
+    public class SalesInvoiceViewModel
+    {
+    }
+    public class SalesSummaryViewModel
+    {
+        public string Month { get; set; }
+        public int MonthCode { get; set; }
+        public int Year { get; set; }
+        public int LastYear { get; set; }
+        public decimal Sales { get; set; }
+        public decimal LastYearSales { get; set; }
+    }
+}
