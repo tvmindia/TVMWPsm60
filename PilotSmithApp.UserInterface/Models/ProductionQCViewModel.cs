382601d1ea2bdf1836da02be5178cc844ed0c150
tvm-dhanil <dhanil.thrithvam@outlook.com>
2018-05-01 11:57:49 +0530

    modification

--- PilotSmithApp.UserInterface/Models/ProductionQCViewModel.cs
+++ PilotSmithApp.UserInterface/Models/ProductionQCViewModel.cs
@@ -9,25 +9,37 @@ namespace PilotSmithApp.UserInterface.Models
     public class ProductionQCViewModel
     {
         public Guid ID { get; set; }
+        [Display(Name ="Quality Control No")]
         public string ProdQCNo { get; set; }
+        [Display(Name = "Quality Control Ref.No")]
         public string ProdQCRefNo { get; set; }
         public DateTime ProdQCDate { get; set; }
         public Guid ProdOrderID { get; set; }
+        [Display(Name = "Select Customer")]
         public Guid CustomerID { get; set; }
+        [Display(Name = "Plant Code")]
         public int PlantCode { get; set; }
+        [Display(Name = "Prepared By")]
         public Guid PreparedBy { get; set; }
+        [Display(Name = "status")]
         public int DocumentStatusCode { get; set; }
+        [Display(Name = "General Notes")]
         public string GeneralNotes { get; set; }
+        [Display(Name = "Document Owner")]
         public Guid DocumentOwnerID { get; set; }
         public bool EmailSentYN { get; set; }
         public Guid LatestApprovalID { get; set; }
         public int LatestApprovalStatus { get; set; }
         public bool IsFinalApproved { get; set; }
         public string EmailSentTo { get; set; }
+        [Display(Name = "Branch")]
         public int BranchCode { get; set; }
         public PSASysCommonViewModel PSASysCommon { get; set; }
         //additional fields
         public string DetailJSON { get; set; }
+        [Display(Name = "Quality Control Date")]
+        [Required(ErrorMessage ="Date is missing")]
+        [RegularExpression("(^(((([1-9])|([0][1-9])|([1-2][0-9])|(30))\\-([A,a][P,p][R,r]|[J,j][U,u][N,n]|[S,s][E,e][P,p]|[N,n][O,o][V,v]))|((([1-9])|([0][1-9])|([1-2][0-9])|([3][0-1]))\\-([J,j][A,a][N,n]|[M,m][A,a][R,r]|[M,m][A,a][Y,y]|[J,j][U,u][L,l]|[A,a][U,u][G,g]|[O,o][C,c][T,t]|[D,d][E,e][C,c])))\\-[0-9]{4}$)|(^(([1-9])|([0][1-9])|([1][0-9])|([2][0-8]))\\-([F,f][E,e][B,b])\\-[0-9]{2}(([02468][1235679])|([13579][01345789]))$)|(^(([1-9])|([0][1-9])|([1][0-9])|([2][0-9]))\\-([F,f][E,e][B,b])\\-[0-9]{2}(([02468][048])|([13579][26]))$)", ErrorMessage = "Date format not accepted")]
         public string ProdQCDateFormatted { get; set; }
         public int TotalCount { get; set; }
         public int FilteredCount { get; set; }
@@ -52,13 +64,21 @@ namespace PilotSmithApp.UserInterface.Models
     {
         public Guid ID { get; set; }
         public Guid ProdQCID { get; set; }
+        [Display(Name ="Product")]
         public Guid ProductID { get; set; }
+        [Display(Name = "Product Model")]
         public Guid ProductModelID { get; set; }
+        [Display(Name = "Product Specification")]
         public string ProductSpec { get; set; }
+        [Display(Name ="Quality Control Qty")]
         public decimal QCQty { get; set; }
         public DateTime QCDate { get; set; }
+        [Display(Name ="Quality Control By")]
         public Guid QCBy { get; set; }
         //additional fields
+        [Display(Name = "Quality Control Date")]
+        [RegularExpression("(^(((([1-9])|([0][1-9])|([1-2][0-9])|(30))\\-([A,a][P,p][R,r]|[J,j][U,u][N,n]|[S,s][E,e][P,p]|[N,n][O,o][V,v]))|((([1-9])|([0][1-9])|([1-2][0-9])|([3][0-1]))\\-([J,j][A,a][N,n]|[M,m][A,a][R,r]|[M,m][A,a][Y,y]|[J,j][U,u][L,l]|[A,a][U,u][G,g]|[O,o][C,c][T,t]|[D,d][E,e][C,c])))\\-[0-9]{4}$)|(^(([1-9])|([0][1-9])|([1][0-9])|([2][0-8]))\\-([F,f][E,e][B,b])\\-[0-9]{2}(([02468][1235679])|([13579][01345789]))$)|(^(([1-9])|([0][1-9])|([1][0-9])|([2][0-9]))\\-([F,f][E,e][B,b])\\-[0-9]{2}(([02468][048])|([13579][26]))$)", ErrorMessage = "Date format not accepted")]
+        public string QCDateFormatted { get; set; }
         public int TotalCount { get; set; }
         public int FilteredCount { get; set; }
         public bool IsUpdate { get; set; }
