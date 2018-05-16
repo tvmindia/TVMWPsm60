63f7665f5253e252fa43570e244392ad2e9c72c2
Jais <Jais@TCMPE>
2018-05-16 09:47:01 +0530

    Reference Type Dropdown in PilotSmith.

--- PilotSmithApp.UserInterface/Controllers/ReferenceTypeController.cs
+++ PilotSmithApp.UserInterface/Controllers/ReferenceTypeController.cs
@@ -30,26 +30,14 @@ namespace PilotSmithApp.UserInterface.Controllers
         }
 
         #region ReferenceType SelectList
-        public ActionResult ReferenceTypeSelectList(string required, bool? disabled)
+        public ActionResult ReferenceTypeSelectList(string required)
         {
             ViewBag.IsRequired = required;
-            ViewBag.IsDisabled = disabled;
-            ViewBag.HasAddPermission = false;
-            ViewBag.propertydisable = disabled == null ? false : disabled;
-            AppUA appUA = Session["AppUA"] as AppUA;
-            Permission permission = _userBusiness.GetSecurityCode(appUA.UserName, "ReferenceType");
-            if (permission.SubPermissionList != null)
-            {
-                if (permission.SubPermissionList.First(s => s.Name == "SelectListAddButton").AccessCode.Contains("R"))
-                {
-                    ViewBag.HasAddPermission = true;
-                }
-            }
             ReferenceTypeViewModel referenceTypeVM = new ReferenceTypeViewModel();
             referenceTypeVM.ReferenceTypeSelectList = _referenceTypeBusiness.GetReferenceTypeSelectList();
             return PartialView("_ReferenceTypeSelectList", referenceTypeVM);
         }
-        #endregion ReferenceType SelectList
+        #endregion
 
     }
 }
