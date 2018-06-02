80cf6dcf5a5e05bf1e7daede758c7ce6400e4b8b
Gibin <gibin.jacob.job@gmail.com>
2018-06-02 17:42:36 +0530

    other charges table  binding

--- PilotSmithApp.UserInterface/Scripts/UserJS/SaleInvoice.js
+++ PilotSmithApp.UserInterface/Scripts/UserJS/SaleInvoice.js
@@ -100,7 +100,7 @@ function BindOrReloadSaleInvoiceTable(action) {
             destroy: true,
             //for performing the import operation after the data loaded
             initComplete: function (settings, json) {
-                debugger;
+               
                 $('.dataTables_wrapper div.bottom div').addClass('col-md-6');
                 $('#tblSaleInvoice').fadeIn(100);
                 if (action == undefined) {
@@ -144,6 +144,7 @@ function AddSaleInvoice() {
     $("#divSaleInvoiceForm").load("SaleInvoice/SaleInvoiceForm?id=" + _emptyGuid, function () {
         ChangeButtonPatchView("SaleInvoice", "btnPatchSaleInvoiceNew", "Add");
         BindSaleInvoiceDetailList(_emptyGuid);
+        BindSaleInvoiceOtherChargesDetailList(_emptyGuid, false);
         OnServerCallComplete();
         //setTimeout(function () {
         //resides in customjs for sliding
@@ -288,10 +289,10 @@ function BindSaleInvoiceDetailList(id, IsSaleOrder, IsQuotation) {
              columns: [
              {
                  "data": "Product.Code", render: function (data, type, row) {
-                     return '<b>Code</b> : ' + data +
+                     return '<div style="width:100%" class="show-popover" data-html="true" data-toggle="popover" data-title="<p align=left>Product Specification" data-content="' + row.ProductSpec.replace(/"/g, "&quot") + '</p>"/>' +
+                         '<b>Code</b> : ' + data +  
                          '</br><b>Name</b> : ' + row.Product.Name +
-                         '</br><b>Model</b> : ' + row.ProductModel.Name +
-                         '</br><b>Spec</b> : ' + row.ProductSpec
+                         '</br><b>Model</b> : ' + row.ProductModel.Name  
                  }, "defaultContent": "<i></i>"
              },
              {
@@ -311,7 +312,7 @@ function BindSaleInvoiceDetailList(id, IsSaleOrder, IsQuotation) {
              },
              {//GST
                "data": "Rate", render: function (data, type, row) {
-                   debugger;
+                 
                    var CGST = parseFloat(row.CGSTPerc != "" ? row.CGSTPerc : 0);
                    var SGST = parseFloat(row.SGSTPerc != "" ? row.SGSTPerc : 0);
                    var IGST = parseFloat(row.IGSTPerc != "" ? row.IGSTPerc : 0);
@@ -403,10 +404,10 @@ function AddSaleInvoiceDetailList() {
 function AddSaleInvoiceDetailToList() {
     debugger;
     $("#FormSaleInvoiceDetail").submit(function () { });
-    debugger;
+  
     if ($('#FormSaleInvoiceDetail #IsUpdate').val() == 'True') {
         if (($('#ProductID').val() != "") && ($('#Rate').val() != "") && ($('#Qty').val() != "") && ($('#UnitCode').val() != "")) {
-            debugger;
+          
             var saleInvoiceDetailList = _dataTable.SaleInvoiceDetailList.rows().data();
             saleInvoiceDetailList[_datatablerowindex].Product.Code = $("#ProductID").val() != "" ? $("#ProductID option:selected").text().split("-")[0].trim() : "";
             saleInvoiceDetailList[_datatablerowindex].Product.Name = $("#ProductID").val() != "" ? $("#ProductID option:selected").text().split("-")[1].trim() : "";
@@ -622,5 +623,116 @@ function DeleteSaleInvoiceFollowup(ID) {
         if (_status == "ERROR") {
             notyAlert('error', _message);
         }
+    }
+}
+
+//OtherExpense
+function BindSaleInvoiceOtherChargesDetailList(id, IsQuotation) {
+    debugger;
+    var data;
+    if (id == _emptyGuid && !(IsQuotation)) {
+        data = null;
+    }
+    else if (id == _emptyGuid && (IsQuotation)) {
+        data = GetSaleInvoiceOtherChargesDetailListBySaleOrderID(id, IsQuotation)
+    }
+    else {
+        data = GetSaleInvoiceOtherChargesDetailListBySaleOrderID(id, IsQuotation)
+    }
+    _dataTable.SaleInvoiceOtherChargesDetailList = $('#tblSaleInvoiceOtherChargesDetailList').DataTable(
+         {
+             dom: '<"pull-right"f>rt<"bottom"ip><"clear">',
+             order: [],
+             searching: false,
+             paging: false,
+             ordering: false,
+             bInfo: false,
+             data:data,
+             language: {
+                 search: "_INPUT_",
+                 searchPlaceholder: "Search"
+             },
+             columns: [
+             { "data": "OtherCharge.Description", render: function (data, type, row) { return data }, "defaultContent": "<i></i>" },
+             { "data": "ChargeAmount", render: function (data, type, row) { return data }, "defaultContent": "<i></i>" },
+             {
+                 "data": "ChargeAmount", render: function (data, type, row) {
+                     debugger;
+                     var CGST = parseFloat(row.CGSTPerc != "" ? row.CGSTPerc : 0);
+                     var SGST = parseFloat(row.SGSTPerc != "" ? row.SGSTPerc : 0);
+                     var IGST = parseFloat(row.IGSTPerc != "" ? row.IGSTPerc : 0);
+                     var CGSTAmt = parseFloat(data * CGST / 100);
+                     var SGSTAmt = parseFloat(data * SGST / 100)
+                     var IGSTAmt = parseFloat(data * IGST / 100)
+                     var GSTAmt = roundoff(parseFloat(CGSTAmt) + parseFloat(SGSTAmt) + parseFloat(IGSTAmt))
+                     return '<div class="show-popover text-right" data-html="true" data-toggle="popover" data-title="<p align=left>Total GST : ₹ ' + GSTAmt + '" data-content=" SGST ' + SGST + '% : ₹ ' + roundoff(parseFloat(SGSTAmt)) + '<br/>CGST ' + CGST + '% : ₹ ' + roundoff(parseFloat(CGSTAmt)) + '<br/> IGST ' + IGST + '% : ₹ ' + roundoff(parseFloat(IGSTAmt)) + '</p>"/>' + GSTAmt
+                 }, "defaultContent": "<i></i>"
+             },
+             {
+                 "data": "ChargeAmount", render: function (data, type, row) {
+                     var CGST = parseFloat(row.CGSTPerc != "" ? row.CGSTPerc : 0);
+                     var SGST = parseFloat(row.SGSTPerc != "" ? row.SGSTPerc : 0);
+                     var IGST = parseFloat(row.IGSTPerc != "" ? row.IGSTPerc : 0);
+                     var CGSTAmt = parseFloat(data * CGST / 100);
+                     var SGSTAmt = parseFloat(data * SGST / 100)
+                     var IGSTAmt = parseFloat(data * IGST / 100)
+                     var GSTAmt = roundoff(parseFloat(CGSTAmt) + parseFloat(SGSTAmt) + parseFloat(IGSTAmt))
+                     var Total = roundoff(parseFloat(data) + parseFloat(GSTAmt))// + parseFloat(row.AddlTaxAmt))
+                    
+                     //return '<div class="show-popover text-right" data-html="true" data-toggle="popover" data-title="<p align=left>Total : ₹ ' + Total + '" data-content="Charge Amount : ₹ ' + data + '<br/>GST : ₹ ' + GSTAmt + '<br/>Additional Tax : ₹ ' + row.AddlTaxAmt + '</p>"/>' + Total
+                     return '<div class="show-popover text-right" data-html="true" data-toggle="popover" data-title="<p align=left>Total : ₹ ' + Total + '" data-content="Charge Amount : ₹ ' + data + '<br/>GST : ₹ ' + GSTAmt +  '</p>"/>' + Total
+                 }, "defaultContent": "<i></i>"
+             },
+             { "data": null, "orderable": false, "defaultContent": '<a href="#" class="actionLink"  onclick="EditSaleOrderOtherChargesDetail(this)" ><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> <a href="#" class="DeleteLink"  onclick="ConfirmDeleteSaleOrderOtherChargeDetail(this)" ><i class="fa fa-trash-o" aria-hidden="true"></i></a>' },
+             ],
+             columnDefs: [
+                 //{ "targets": [0], "width": "30%" },
+                 //{ "targets": [1, 2, 3, 4], "width": "15%" },
+                 //{ "targets": [5], "width": "10%" },
+                 { className: "text-left", "targets": [0] },
+                 { className: "text-right", "targets": [1, 2, 3] },
+                 { className: "text-center", "targets": [4] }
+             ],
+             destroy: true,
+         });
+    $('[data-toggle="popover"]').popover({
+        html: true,
+        'trigger': 'hover',
+        'placement': 'top'
+    });
+}
+
+function GetSaleInvoiceOtherChargesDetailListBySaleOrderID(id, IsQuotation) {
+    try {
+        debugger;
+
+        var SaleInvoiceOtherChargesDetailList = [];
+        if (IsQuotation) {
+            debugger;
+            var data = { "quotationID": $('#SaleInvoiceForm #hdnQuoteID').val() };
+            _jsonData = GetDataFromServer("SaleInvoice/GetQuotationOtherChargesDetailListByQuotationID/", data);
+        }
+        else {
+            var data = { "saleOrderID": id };
+            _jsonData = GetDataFromServer("SaleInvoice/GetSaleInvoiceOtherChargesDetailListBySaleOrderID/", data);
+        }
+
+        if (_jsonData != '') {
+            _jsonData = JSON.parse(_jsonData);
+            _message = _jsonData.Message;
+            _status = _jsonData.Status;
+            SaleInvoiceOtherChargesDetailList = _jsonData.Records;
+        }
+        if (_status == "OK") {
+            return SaleInvoiceOtherChargesDetailList;
+        }
+        if (_status == "ERROR") {
+            notyAlert('error', _message);
+        }
+    }
+    catch (e) {
+        //this will show the error msg in the browser console(F12) 
+        console.log(e.message);
+
     }
 }
