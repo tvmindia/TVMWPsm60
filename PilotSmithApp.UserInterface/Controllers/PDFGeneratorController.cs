0cd8d61e76b7c92de1e383414bc85636ac5cd4a3
Thomson Varkey <thomson.thrithvam@outlook.com>
2018-06-11 15:29:03 +0530

    Added changes with pdf

--- PilotSmithApp.UserInterface/Controllers/PDFGeneratorController.cs
+++ PilotSmithApp.UserInterface/Controllers/PDFGeneratorController.cs
@@ -47,7 +47,7 @@ namespace PilotSmithApp.UserInterface.Controllers
                 throw ex;
             }
         }
-        public string PrintPDF(PDFToolsViewModel pDFToolsObj)
+        public string PrintPDF(PDFTools pDFToolsObj)
         {
             //string imageURL = Server.MapPath("~/Content/images/logo.png");
             //iTextSharp.text.Image jpg = iTextSharp.text.Image.GetInstance(imageURL);
@@ -65,7 +65,7 @@ namespace PilotSmithApp.UserInterface.Controllers
             {
                 PdfWriter writer = PdfWriter.GetInstance(pdfDoc, memoryStream);
                 Footer footobj = new Footer();
-                footobj.imageURL=Server.MapPath("~/Content/images/logo.png");
+                footobj.imageURL = Server.MapPath("~/Content/images/logo.png");
                 footobj.Header = XMLWorkerHelper.ParseToElementList(pDFToolsObj.Headcontent, null);
                 writer.PageEvent = footobj;
                 // Our custom Header and Footer is done using Event Handler
@@ -95,7 +95,7 @@ namespace PilotSmithApp.UserInterface.Controllers
                 // Our custom Header and Footer is done using Event Handler
 
                 //pdfDoc.Add(welcomeParagraph);
-                
+
                 XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
                 //for (int i = 0; i <= 2; i++)
                 //{
@@ -108,7 +108,7 @@ namespace PilotSmithApp.UserInterface.Controllers
                 //}
                 pdfDoc.Close();
                 bytes = memoryStream.ToArray();
-                memoryStream.Close();                
+                memoryStream.Close();
             }
             string fname = Path.Combine(Server.MapPath("~/Content/Uploads/"), "Report.pdf");
             System.IO.File.WriteAllBytes(fname, bytes);
@@ -117,7 +117,7 @@ namespace PilotSmithApp.UserInterface.Controllers
             return JsonConvert.SerializeObject(new { Result = "OK", URL = "../Content/Uploads/Report.pdf" });
 
         }
-         public partial class Footer : PdfPageEventHelper
+        public partial class Footer : PdfPageEventHelper
 
         {
             public string imageURL { get; set; }
@@ -126,84 +126,112 @@ namespace PilotSmithApp.UserInterface.Controllers
             public override void OnEndPage(PdfWriter writer, Document doc)
 
             {
+                var FontColour = new BaseColor(0, 100, 0);
+                var FontColour1 = new BaseColor(0, 100, 3);
+                Font myFont1 = FontFactory.GetFont("OpenSans", 7, iTextSharp.text.Font.BOLD, FontColour1);
                 PSASysCommon pSASSysCommon = new PSASysCommon();
-                Paragraph footer = new Paragraph("This is a Computer Generated Document on: " + pSASSysCommon.GetCurrentDateTime().ToString("dd-MMM-yyyy h:mm tt"), FontFactory.GetFont(FontFactory.HELVETICA, 8, iTextSharp.text.Font.ITALIC));
-                footer.Alignment = Element.ALIGN_RIGHT;
-                PdfPTable footerTbl = new PdfPTable(1);
-                footerTbl.TotalWidth = 400;
-                footerTbl.HorizontalAlignment = Element.ALIGN_CENTER;
-                PdfPCell cell = new PdfPCell(footer);
-                cell.Border = 0;
-                cell.PaddingLeft = 10;
-                footerTbl.AddCell(cell);
-                footerTbl.WriteSelectedRows(0, -1,250, 30, writer.DirectContent);
+                //Paragraph footer = new Paragraph("Generated on: "+ pSASSysCommon.GetCurrentDateTime().ToString("dd-MMM-yyyy h:mm tt"), FontFactory.GetFont(FontFactory.HELVETICA, 8, iTextSharp.text.Font.ITALIC));
+                //footer.Alignment = Element.ALIGN_RIGHT;
+                PdfPTable footerTbl = new PdfPTable(3);
+                footerTbl.DefaultCell.Border = Rectangle.NO_BORDER;
+                float[] ColumnWidths = new float[] { doc.PageSize.Width / 3, doc.PageSize.Width / 3, doc.PageSize.Width / 3 };
+                footerTbl.SetWidths(ColumnWidths);
+                footerTbl.TotalWidth = doc.PageSize.Width;
+                Paragraph p = new Paragraph(new Chunk(new LineSeparator(34.01F, 102.0F, FontColour, Element.ALIGN_LEFT, 0)));
+                PdfPCell cellLine = new PdfPCell(p);
+                cellLine.Colspan = 3;
+                cellLine.MinimumHeight = 34.01F;
+                cellLine.Border = Rectangle.NO_BORDER;
+                cellLine.PaddingLeft = -10;
+                Phrase ph1 = new Phrase("H.O.,Demonstration Lab and Works\nKallettumkara.P.O,Thrissur-680683\nKerala(India)\nTelefax :+91 480 2881157,2881908\nEmail :mech@pilotsmithindia.com\nRly. Station : Irinjalakuda (0.5Km)\nAirport(25 Km),Seaport(55 Km) : Kochi\nGSTIN : 32AADCP8777H1Z3\nCIN No. U28939TN2005PTC057413", myFont1);
+                PdfPCell cellLine1 = new PdfPCell(ph1);
+                cellLine1.MinimumHeight = 90.7087F;
+                cellLine1.PaddingLeft = 50.51394F;
+                cellLine1.Border = Rectangle.NO_BORDER;
+                Phrase ph2 = new Phrase("Registered Office and Showroom\nNew No. I,Gajapathy Street,Shenoy nagar,Chennai-600030\nTamil Nadu(India)\nTelefax :+91 44 26640966,09381078554\nEmail :madras@pilotsmithindia.com\nRly. Station : Chennai Central (6 Km)\nAirport(15 Km),Seaport(10 Km) :Chennai \nGSTIN : 33AADCP8777H1Z1", myFont1);
+                PdfPCell cellLine2 = new PdfPCell(ph2);
+                cellLine2.MinimumHeight = 90.7087F;
+                cellLine2.PaddingLeft = 8.50394F;
+                cellLine2.BorderColor = FontColour;
+                cellLine2.Border = Rectangle.LEFT_BORDER;
+                Phrase ph3 = new Phrase("Branch Office and Showroom\nFirst Floor,No. 691,10th Main Road\nVinayaka Layout, Nagar Bhavi\nBengaluru - 560072\nKarnataka(India)\nTelefax: +91 9108001798, 9108001799 \nEmail: bangalore@pilotsmithindia.com\nRly.Station : Bangalore City Jn(11 Km)\nAirport(40 Km) :Bengaluru \nGSTIN: 29AADCP8777H1ZQ", myFont1);
+                PdfPCell cellLine3 = new PdfPCell(ph3);//8.50394
+                cellLine3.MinimumHeight = 90.7087F;
+                cellLine3.PaddingLeft = 8.50394F;
+                cellLine3.BorderColor = FontColour;
+                cellLine3.Border = Rectangle.LEFT_BORDER;
+                footerTbl.AddCell(cellLine1);
+                footerTbl.AddCell(cellLine2);
+                footerTbl.AddCell(cellLine3);
+                footerTbl.AddCell(cellLine);
+                footerTbl.WriteSelectedRows(0, -1, 0, 121.732f, writer.DirectContent);
 
             }
             public override void OnStartPage(PdfWriter writer, Document document)
             {
 
-                //iTextSharp.text.Image jpg = iTextSharp.text.Image.GetInstance(imageURL);
-                ////Resize image depend upon your need
-                //jpg.ScaleToFit(90.70f, 70.86f);
-                ////jpg.SpacingBefore = 39.68f;
-                ////jpg.SpacingAfter = 22.67f;
-                //jpg.Alignment = Element.ALIGN_LEFT;
-                ////jpg.SetAbsolutePosition(document.Left, document.Top - 60);
-                //Font myFont = FontFactory.GetFont(FontFactory.TIMES_BOLD, 14, iTextSharp.text.Font.BOLD);
-                //Font myFont1 = FontFactory.GetFont(FontFactory.TIMES_BOLD, 10, iTextSharp.text.Font.NORMAL);
-                //Font myFont2 = FontFactory.GetFont(FontFactory.TIMES_BOLD, 12, iTextSharp.text.Font.BOLDITALIC);
-                //string line1 = "Pilotsmith (India) Pvt.Ltd." + "\n";
-                //string line2 = "www.pilotsmithindia.com" + "\n";
-                //string line3 = "\n"+"Manufacturers and Consultants for Food and Ayurvedic Processing Equipments." + "\n";
-                //Paragraph header = new Paragraph();
-                //Phrase ph1 = new Phrase(line1, myFont);
-                //Phrase ph2 = new Phrase(line2, myFont1);
-                //Phrase ph3 = new Phrase(line3, myFont2);
-                //header.Add(ph1);
-                //header.Add(ph2);
-                //header.Add(ph3);
-                //header.Alignment = Element.ALIGN_LEFT;
-                //PdfPTable headerTbl = new PdfPTable(2);
-                //headerTbl.TotalWidth = document.PageSize.Width;
-                ////headerTbl.HeaderHeight = 60;
-                //headerTbl.HorizontalAlignment = Element.ALIGN_LEFT;
-                //float[] widths = new float[] { 90.70f, document.PageSize.Width - 90.70f };
-                //headerTbl.SetWidths(widths);
-                //PdfPCell cell = new PdfPCell(jpg);
-                //cell.Border = 0;
-                //cell.PaddingLeft = 10;
-                //headerTbl.AddCell(cell);
-                //PdfPCell cell1 = new PdfPCell(header);
-                //cell1.Border = 0;
-                //cell1.PaddingLeft = 50;
-                ////cell1.Width = document.PageSize.Width - 90;
-                //headerTbl.AddCell(cell1);
-                ////ColumnText ct = new ColumnText(writer.DirectContent);
-                ////ct.SetSimpleColumn(new Rectangle(10, 790, 559, 600));
-                ////foreach (IElement e in Header)
-                ////{
-                ////    ct.AddElement(e);
-                ////}
-                ////ct.Go();
-
-                //headerTbl.WriteSelectedRows(0, -1, 0, 832, writer.DirectContent);
+                BaseFont customfont = BaseFont.CreateFont(System.Web.HttpContext.Current.Server.MapPath("~/fonts/OpenSans-Regular.ttf"), BaseFont.CP1252, BaseFont.EMBEDDED);
+                Font font = new Font(customfont);
+                var FontColour = new BaseColor(0, 100, 0);
+                var FontColour1 = new BaseColor(0, 100, 3);
+                PdfPTable headerSectionTbl = new PdfPTable(2);
+                headerSectionTbl.DefaultCell.Border = Rectangle.NO_BORDER;
+                float[] ColumnWidths = new float[] { 119.055F, document.PageSize.Width - 119.055F };
+                headerSectionTbl.SetWidths(ColumnWidths);
+                headerSectionTbl.TotalWidth = document.PageSize.Width;
+                Paragraph p = new Paragraph(new Chunk(new LineSeparator(34.01F, 102.0F, FontColour, Element.ALIGN_LEFT, 0)));
+                PdfPCell cellLine = new PdfPCell(p);
+                cellLine.Colspan = 2;
+                cellLine.MinimumHeight = 34.01F;
+                cellLine.Border = Rectangle.NO_BORDER;
+                cellLine.PaddingLeft = -10;
+                headerSectionTbl.AddCell(cellLine);
+                iTextSharp.text.Image jpg = iTextSharp.text.Image.GetInstance(imageURL);
+                //Resize image depend upon your need
+                jpg.ScaleToFit(76.5354f, 93.5433f);
+                jpg.SpacingBefore = 14.1732F;
+                jpg.Alignment = Element.ALIGN_RIGHT;
+                Font myFont = FontFactory.GetFont("OpenSans", 12, iTextSharp.text.Font.BOLD, FontColour);
+                Font myFont1 = FontFactory.GetFont("OpenSans", 9, iTextSharp.text.Font.BOLD, FontColour1);
+                Font myFont2 = FontFactory.GetFont("OpenSans", 10, iTextSharp.text.Font.BOLDITALIC, FontColour1);
+                string line1 = "\nPilotsmith (India) Pvt.Ltd." + "\n";
+                string line2 = "www.pilotsmithindia.com" + "\n";
+                string line3 = "\n" + "Manufacturers and Consultants for Food and Ayurvedic Processing Equipments." + "\n";
+                Paragraph header = new Paragraph();
+                Phrase ph1 = new Phrase(line1, myFont);
+                Phrase ph2 = new Phrase(line2, myFont1);
+                Phrase ph3 = new Phrase(line3, myFont2);
+                header.Add(ph1);
+                header.Add(ph2);
+                header.Add(ph3);
+                header.SpacingBefore = 14.1732F;
+                header.Alignment = Element.ALIGN_LEFT;
+                PdfPCell cellImage = new PdfPCell(jpg);
+                cellImage.Border = Rectangle.NO_BORDER;
+                cellImage.PaddingLeft = 42.5197F;
+                headerSectionTbl.AddCell(cellImage);
+                PdfPCell cell1 = new PdfPCell(header);
+                cell1.Border = 0;
+                cell1.PaddingLeft = 19.8425F;
+                headerSectionTbl.AddCell(cell1);
+                headerSectionTbl.WriteSelectedRows(0, -1, 0, document.PageSize.Height, writer.DirectContent);
             }
-            }
+        }
         [HttpPost]
-        public FileResult Download(PDFToolsViewModel PDFTools)
+        public FileResult Download(PDFTools PDFTools)
         {
-           // Footer footobj = new Footer();
-            
+            // Footer footobj = new Footer();
+
             //jpg.Alignment = Element.ALIGN_LEFT;
             string htmlBody = PDFTools.Content == null ? "" : PDFTools.Content.Replace("<br>", "<br/>").ToString().Replace("workAround:image\">", "workAround:image\"/>");
             StringReader reader = new StringReader(htmlBody.ToString());
-            Document pdfDoc = new Document(PageSize.A4, -13f, -4f, 30f, 100f);
+            Document pdfDoc = new Document(PageSize.A4, 28.3465f, 28.3465f, 141.732f, 141.732f);
             byte[] bytes = null;
             using (MemoryStream memoryStream = new MemoryStream())
             {
                 PdfWriter writer = PdfWriter.GetInstance(pdfDoc, memoryStream);
                 Footer footobj = new Footer();
-                footobj.imageURL = Server.MapPath("~/Content/images/logo2.png");
+                footobj.imageURL = Server.MapPath("~/Content/images/PilotLogo.png");
                 writer.PageEvent = footobj;
                 pdfDoc.Open();
                 XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, reader);
@@ -218,7 +246,9 @@ namespace PilotSmithApp.UserInterface.Controllers
             return File(fname, contentType, contentFileName);
         }
 
+
+
     }
-       
-    }
+
+}
 
