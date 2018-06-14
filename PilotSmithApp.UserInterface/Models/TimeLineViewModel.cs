7c094ec8c9775f22af499149c9a1b4903b25ec75
Suvaneeth <Suvaneeth@DESKTOP-LCFSGU0>
2018-06-14 17:59:50 +0530

    timeline classes added

---
+++ PilotSmithApp.UserInterface/Models/TimeLineViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PilotSmithApp.UserInterface.Models
+{
+    public class TimeLineViewModel
+    {
+        public String DocumentType { get; set; }
+        public Guid DocumentID { get; set; }
+        public String DocumentNo { get; set; }
+        public DateTime DocumentDate { get; set; }
+        public string URL { get; set; }
+    }
+}
