8a1da0bf300f4ca1b7e80c6a60366a5b26aad37b
tvm-dhanil <dhanil.thrithvam@outlook.com>
2018-09-24 14:16:18 +0530

    PLT-0043,0049,0048,0055 bug fixes

--- PilotSmithApp.UserInterface/Scripts/UserJS/Report/PendingProductionOrderReport.js
+++ PilotSmithApp.UserInterface/Scripts/UserJS/Report/PendingProductionOrderReport.js
@@ -199,6 +199,9 @@ function BindOrReloadPendingProductionOrderReportTable(action) {
             paging: true,
             lengthChange: false,
             autoWidth: false,
+            scrollX: "500px",
+            fixedHeader: true,
+            //scrollCollapse: true,
             processing: true,
             language: {
                 "processing": "<div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div>"
